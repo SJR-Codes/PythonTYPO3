5becff8ad87c14cca08eee6a5df5bd5cb0050fa7
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-13 14:03:08 +0300

    initialize kurssin_tulokset

---
+++ 20221013_jutut/kurssin_tulokset.py
@@ -0,0 +1,13 @@
+"""
+* Taitotalo Python 13.10.2022
+* kurssin_tulokset.py
+* description
+* Created by Samu Reinikainen
+"""
+
+
+
+
+# run blocks of code only if our program is the main program executed
+if __name__ == "__main__":
+    main()
