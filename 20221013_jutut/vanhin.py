95531408c35e634dd75cad8734a8d2148859a3bb
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-13 11:47:28 +0300

    updated function to use lambda

--- 20221013_jutut/vanhin.py
+++ 20221013_jutut/vanhin.py
@@ -2,9 +2,13 @@ def vanhin(lista: list) -> str:
     '''Find oldest person on tuples-list and return their name'''
 
     #change tuples list to dictionary
-    new = {lista[i][0]: lista[i][1] for i in range(len(lista))}    
+    #new = {lista[i][0]: lista[i][1] for i in range(len(lista))}    
     #print(new)
-    return min(new, key=new.get)
+    #return min(new, key=new.get)
+
+    #using lambda
+    new = min(lista, key=lambda x: x[1])
+    return(new[0])
 
 h1 = ("Arto", 1977)
 h2 = ("Einari", 1985)
